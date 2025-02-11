bc16d1c06832a4e357f5684324759526dfedef8d
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-11 23:50:09 +0000

    re-encrypted secrets

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-gpg -r "Norbert Majer" --decrypt ./secrets/encrypted_secrets/terraform.tfvars.gpg > ./terraform/terraform.tfvars
+gpg -r "Norbert Majer" -r "Norbert Majer 2" --decrypt ./secrets/encrypted_secrets/terraform.tfvars.gpg > ./terraform/terraform.tfvars
---
+++ scripts/encrypt.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gpg -r "Norbert Majer" -r "Norbert Majer 2" --encrypt ./terraform/terraform.tfvars
