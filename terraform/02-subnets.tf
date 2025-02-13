4c74a0fe8be32c3c30d4f31761837d86802639e6
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-13 20:32:13 +0000

    add private subnets

---
+++ terraform/02-subnets.tf
@@ -0,0 +1,30 @@
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.private-subnets-cidr)
+  cidr_block        = element(var.private-subnets-cidr, count.index)
+  vpc_id            = aws_vpc.eks_portfolio_vpc.id
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name = format("private-subnet-%s", count.index),
+    # "kubernetes.io/role/internal-elb" = "1",
+    # "kubernetes.io/cluster/demo"      = "owned",
+    finops_billing_code       = var.finops_billing_code,
+    servicenow_application_id = var.servicenow_application_id
+
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public-subnets-cidr)
+  cidr_block              = element(var.public-subnets-cidr, count.index)
+  vpc_id                  = aws_vpc.eks_portfolio_vpc.id
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = format("public-subnet-%s", count.index),
+    # "kubernetes.io/role/internal-elb" = "1",
+    # "kubernetes.io/cluster/demo"      = "owned",
+    finops_billing_code       = var.finops_billing_code,
+    servicenow_application_id = var.servicenow_application_id
+
+  }
+}
