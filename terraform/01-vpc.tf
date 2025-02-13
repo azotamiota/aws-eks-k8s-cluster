0274d8e265e490e2662d5cf0231c71edffc01d6f
azotamiota <azotamiota@gmail.com>
2025-02-13 23:20:10 +0000

    formatting

--- terraform/01-vpc.tf
+++ terraform/01-vpc.tf
@@ -1,4 +1,4 @@
 resource "aws_vpc" "eks_portfolio_vpc" {
   cidr_block = "10.240.0.0/16"
-  tags = merge(tomap({"Name"= "eks-portfolio-vpc"}), var.permanent_tags)
+  tags       = merge(tomap({ "Name" = "eks-portfolio-vpc" }), var.permanent_tags)
 }
--- terraform/02-subnets.tf
+++ terraform/02-subnets.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "private_subnet" {
   cidr_block        = element(var.private-subnets-cidr, count.index)
   vpc_id            = aws_vpc.eks_portfolio_vpc.id
   availability_zone = element(var.availability_zones, count.index)
-  tags = merge(tomap({"Name" = format("private-subnet-%s", count.index)}), var.permanent_tags)
+  tags              = merge(tomap({ "Name" = format("private-subnet-%s", count.index) }), var.permanent_tags)
 }
 
 resource "aws_subnet" "public_subnet" {
@@ -12,5 +12,5 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.eks_portfolio_vpc.id
   availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
-  tags = merge(tomap({"Name" = format("public-subnet-%s", count.index)}), var.permanent_tags)
+  tags                    = merge(tomap({ "Name" = format("public-subnet-%s", count.index) }), var.permanent_tags)
 }
