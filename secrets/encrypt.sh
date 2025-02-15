415ec05e9181c9aa8503b1d751bed7d55db0fe96
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-15 18:22:19 +0000

    update secret management

--- secrets/encrypt.sh
+++ secrets/encrypt.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-gpg -r "Norbert Majer" -r "Norbert Majer 2" --encrypt ./terraform/terraform.tfvars
-mv ./terraform/terraform.tfvars.gpg ./secrets/encrypted_secrets/
+
+./secrets/import.sh
+
+users=$(for i in secrets/recipients/*.asc ; do gpg --keyid-format 0xlong --import-options show-only "${i}" | perl -nle 's/^\s*//g; print unless /^(pub|uid|sub)/;' ; done)
+
+recipients=""
+for recipient in $users; do
+  recipients="${recipients} --recipient ${recipient} "
+done
+
+gpg --batch --yes --always-trust --output "secrets/encrypted_secrets/terraform.tfvars.encrypted" ${recipients} --encrypt ./terraform/terraform.tfvars
+
+# gpg -r "Norbert Majer" -r "Norbert Majer 2" --encrypt ./terraform/terraform.tfvars
+# mv ./terraform/terraform.tfvars.gpg ./secrets/encrypted_secrets/
--- secrets/import.sh
+++ secrets/import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for public_key in secrets/users/*.asc; do
-  echo "ehune: $public_key"
+for public_key in secrets/recipients/*.asc; do
+   gpg --quiet --import "$public_key"
 done
