67eee288bf7e6d2b948b46be5c1d519ecd042a38
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-17 15:52:34 +0000

    re-configure backend

--- terraform/00-provider.tf
+++ terraform/00-provider.tf
@@ -8,15 +8,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.86"
     }
   }
   required_version = ">= 0.12"
-  # backend "s3" {
-  #   bucket         = "eks-portfolio-s3-state-bucket"
-  #   key            = "terraform-state/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-lock-table-eks-portfolio"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "portfolio-s3-backend-state-bucket"
+    key            = "terraform-state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table-eks-portfolio"
+    encrypt        = true
+  }
 }
