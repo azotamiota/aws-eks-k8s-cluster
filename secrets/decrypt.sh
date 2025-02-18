6c2d8e0e6523654309b0881de89cf26859513766
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-18 16:45:15 +0000

    add decrypt script

--- secrets/decrypt.sh
+++ secrets/decrypt.sh
@@ -2,4 +2,6 @@
 
 ./secrets/import.sh
 
+echo "Decrypting secrets..."
 gpg --batch --yes --output "./terraform/terraform.tfvars" --decrypt ./secrets/encrypted_secrets/terraform.tfvars.encrypted
+echo "Done."
