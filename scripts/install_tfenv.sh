b213474e7f4c74e46afb04b3f165929b52f955a0
azotamiota <azotamiota@gmail.com>
2025-02-18 21:43:24 +0000

    check decrypted terraform.tfvars

--- scripts/install_tfenv.sh
+++ scripts/install_tfenv.sh
@@ -5,5 +5,5 @@ git clone https://github.com/kamatama41/tfenv.git /usr/share/.tfenv &&
 ln -s /usr/share/.tfenv/bin/* /usr/local/bin/
 
 tfenv install latest
-tfenv install 0.12.21 
-tfenv use 0.12.21
+# tfenv install 0.12.21 
+# tfenv use 0.12.21
