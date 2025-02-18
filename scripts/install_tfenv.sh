aad4341b534a72d482e225f1c44507826cc7ba27
azotamiota <azotamiota@gmail.com>
2025-02-18 22:01:54 +0000

    install certain version tfenv

--- scripts/install_tfenv.sh
+++ scripts/install_tfenv.sh
@@ -4,6 +4,6 @@
 git clone https://github.com/kamatama41/tfenv.git /usr/share/.tfenv &&
 ln -s /usr/share/.tfenv/bin/* /usr/local/bin/
 
-tfenv install latest
-# tfenv install 0.12.21 
-# tfenv use 0.12.21
+# tfenv install latest
+tfenv install 1.10.5 
+tfenv use 1.10.5
