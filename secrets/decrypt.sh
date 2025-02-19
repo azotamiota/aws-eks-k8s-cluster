1a06f07af49eb82d6c6f43ef2da31ee0504ee908
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-19 23:15:04 +0000

    decrypting silently

--- secrets/decrypt.sh
+++ secrets/decrypt.sh
@@ -3,5 +3,5 @@
 ./secrets/import.sh
 
 echo "Decrypting secrets..."
-gpg --batch --yes --output "./terraform/terraform.tfvars" --decrypt ./secrets/encrypted_secrets/terraform.tfvars.encrypted
+gpg --batch --yes --quiet --output "./terraform/terraform.tfvars" --decrypt ./secrets/encrypted_secrets/terraform.tfvars.encrypted
 echo "Done."
