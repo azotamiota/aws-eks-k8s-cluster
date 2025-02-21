5738561f30692cc6ba229dd4d3a0bd4c11581fc3
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-21 21:49:54 +0000

    working setup with modules

--- terraform/06-nat-gateway.tf
+++ terraform/06-nat-gateway.tf
@@ -1,12 +1,12 @@
-resource "aws_eip" "nat_elastic_ip" {
-  domain = "vpc"
-  tags   = merge(tomap({ "Name" = "nat-elastic-ip" }), var.permanent_tags)
-}
+# resource "aws_eip" "nat_elastic_ip" {
+#   domain = "vpc"
+#   tags   = merge(tomap({ "Name" = "nat-elastic-ip" }), var.permanent_tags)
+# }
 
-resource "aws_nat_gateway" "eks_portfolio_nat_gw" {
-  allocation_id = aws_eip.nat_elastic_ip.id
-  subnet_id     = aws_subnet.public_subnet[0].id
-  tags          = merge(tomap({ "Name" = "eks_portfolio_nat_gw" }), var.permanent_tags)
+# resource "aws_nat_gateway" "eks_portfolio_nat_gw" {
+#   allocation_id = aws_eip.nat_elastic_ip.id
+#   subnet_id     = aws_subnet.public_subnet[0].id
+#   tags          = merge(tomap({ "Name" = "eks_portfolio_nat_gw" }), var.permanent_tags)
 
-  depends_on = [aws_internet_gateway.eks_portfolio_igw]
-}
+#   depends_on = [aws_internet_gateway.eks_portfolio_igw]
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,24 @@
-variable "private-subnets-cidr" {
-  description = "Private subnets CIDR block"
-  type        = list(string)
-  default     = ["10.240.0.0/19", "10.240.32.0/19"]
-}
+# variable "private-subnets-cidr" {
+#   description = "Private subnets CIDR block"
+#   type        = list(string)
+#   default     = ["10.240.0.0/19", "10.240.32.0/19"]
+# }
 
-variable "public-subnets-cidr" {
-  description = "Public subnets CIDR block"
-  type        = list(string)
-  default     = ["10.240.64.0/19", "10.240.96.0/19"]
-}
+# variable "public-subnets-cidr" {
+#   description = "Public subnets CIDR block"
+#   type        = list(string)
+#   default     = ["10.240.64.0/19", "10.240.96.0/19"]
+# }
 
-variable "availability_zones" {
-  description = "Availibility zones"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+# variable "availability_zones" {
+#   description = "Availibility zones"
+#   type        = list(string)
+#   default     = ["us-east-1a", "us-east-1b"]
+# }
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
 }
 
 variable "permanent_tags" {
