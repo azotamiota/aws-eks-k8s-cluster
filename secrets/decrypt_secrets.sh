20c4a2f83f39d932f61a566f5633f0397e48504a
Norbert Majer <norbert.majer2@kingfisher.com>
2025-02-21 12:36:11 +0000

    bugfix

--- secrets/decrypt_secrets.sh
+++ secrets/decrypt_secrets.sh
@@ -3,6 +3,7 @@
 ./secrets/import_gpg_keys.sh
 
 decrypted_path="secrets/decrypted_secrets/"
+mkdir -p ./secrets/decrypted_secrets
 for path_to_secret in secrets/encrypted_secrets/*.encrypted; do
   encrypted_file=$(echo "$path_to_secret" | tr '/' ' ' | awk '{print $3}')
   basename="${encrypted_file%.encrypted}"
